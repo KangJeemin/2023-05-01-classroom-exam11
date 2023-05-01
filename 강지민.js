ac3b6da3b80fc5f2f2754973cbde8f095d36d2e8
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-05-01 22:32:49 +0900

    js 배열 검사하는거 typeof 말고 isarray 썼던 것 같은데, 그리고 파이썬에서 for in 은 Js에서 foreach

--- 강지민.js
+++ 강지민.js
@@ -1,1 +1,13 @@
-let example = [[1,2,3],[4,[5,6]],7,[8,9]]
+let example = [[1,2,3],[4,[5,6]],7,[8,9]];
+
+function flatten(data) {
+    let output = [];
+
+    data.forEach(element => {
+        if(typeof element=="array") {
+            output +=element;
+
+        }
+    });
+
+}
